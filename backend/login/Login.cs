d564d12dd45d951f7316b2eb91e96c5b349ae4f3
Futagirl <anime@incest.rocks>
2022-09-15 11:32:33 +0200

    Loginsachen

--- backend/login/Login.cs
+++ backend/login/Login.cs
@@ -1,14 +1,28 @@
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.Http.Logging;
+
 namespace awl_raumreservierung{
 	public static class Login{
-		// public static bool CheckLogin(string user, string password){
-		// 	// User user; // User mit username holen
+		public static async LoginMessage CheckLogin(string username, string password){
+			var user = new Class()
+							.User
+							.Where(u => u.Username == username)
+							.FirstOrDefault();
 
-		// 	// if(user)
+			 // User mit username holen
+			return user switch 
+			{
+				{Active: 1} => LoginMessage.InactiveUser,
+				{Passwd: var pw} when BC.Verify(password, pw) => LoginMessage.Success,
+				_ => LoginMessage.InvalidCredentials
+			};
+		}
 
-		// 	if (BC.Verify("my password", "passwordHash")){
-				
-		// 	}
+		public enum LoginMessage {
+			InvalidCredentials,
+			Success,
+			InactiveUser
 
-		// }
+		}
 	}
 }
