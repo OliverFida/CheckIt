4c20854367d7fbcedd3d88dfb0deea1094b4ca84
Markus Hatzenberger <markusH@intern.hollaus.at>
2022-09-15 10:50:08 +0200

    Klassen fuer Datenbank hinzugefuegto

---
+++ backend/classes/Class.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+
+public class Class : DbContext
+{
+	public DbSet<User> User { get; set; }
+	public DbSet<Room> Room { get; set; }
+	public DbSet<Bookings> Bookings { get; set; }
+
+	public string DbPath { get; }
+
+	public Class()
+	{
+		var folder = Environment.SpecialFolder.LocalApplicationData;
+		var path = Environment.GetFolderPath(folder);
+		DbPath = System.IO.Path.Join(path, "checkIT.db");
+	}
+	protected override void OnConfiguring(DbContextOptionsBuilder options)
+		 => options.UseSqlite($"Data Source={DbPath}");
+}
+
+public class Bookings
+{
+	public int BookingId { get; set; }
+	public int StartTime { get; set; }
+	public int EndTime { get; set; }
+	public int Room { get; set; }
+	public int UserID { get; set; }
+	public int CreateTime { get; set; }
+	public int CreatedBy { get; set; }
+
+}
+
+public class User
+{
+	public int UserId { get; set; }
+	public string Username { get; set; }
+	public string Firstname { get; set; }
+	public string LastName { get; set; }
+	public string Passwd { get; set; }
+	public int LastLogon { get; set; }
+	public int LastChange { get; set; }
+	public int Active { get; set; }
+	public int Role { get; set; }
+}
+public class Room
+{
+	public int RoomID { get; set; }
+	public string Number { get; set; }
+	public string Name { get; set; }
+}
---
+++ backend/classes/accessDB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+internal class accessDB
+{
+	private static void Main(string[] args)
+	{
+		var db = new Class();
+
+
+
+	}
+}
