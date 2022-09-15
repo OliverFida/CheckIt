a752ec0903a2ff2db3cabc94bfef183408e18029
OliverFida <oliver.fi99@gmail.com>
2022-09-15 11:29:17 +0200

    HomePage Erste Idee

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,5 +1,28 @@
+// Component imports
+import {Layout, Space, Button} from 'antd';
+// Style imports
+import '../css/pages/HomePage.css';
+
+const {Header, Content} = Layout;
+
 export default function HomePage(){
     return(
-        <h1>Home</h1>
+        <Layout>
+            <AppHeader />
+            <Content>
+                
+            </Content>
+        </Layout>
     );
 };
+
+function AppHeader(){
+    return(
+        <Header id="Header" theme='light'>
+            <Space direction='horizontal'>
+                <h1>CHECK-IT</h1>
+                <Button type='ghost'>Abmelden</Button>
+            </Space>
+        </Header>
+    );
+}
