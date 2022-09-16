7b93451e56c37895b23c0349f39dd166b193daf6
Futagirl <anime@incest.rocks>
2022-09-16 09:11:48 +0200

    Adminsachen und  Rollen

--- backend/Controllers/loginController.cs
+++ backend/Controllers/loginController.cs
@@ -1,11 +1,10 @@
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using Microsoft.AspNetCore.Mvc;
-
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
 
 namespace awl_raumreservierung.Controllers;
 
@@ -13,57 +12,70 @@ namespace awl_raumreservierung.Controllers;
 [Route("[controller]")]
 public class loginController : ControllerBase
 {
-    public static WebApplicationBuilder? builder;
-    private readonly ILogger<loginController> _logger;
+	public static WebApplicationBuilder? builder;
+	private readonly ILogger<loginController> _logger;
 
-    public loginController(ILogger<loginController> logger)
-    {
-        _logger = logger;
-    }
+	public loginController(ILogger<loginController> logger)
+	{
+		_logger = logger;
+	}
 
-    [HttpPost(Name = "login")]
-    public IResult Post(string username, string password){
-        var res = Login.CheckLogin(username, password);
-        
-        var statuscode = res switch {
-            Login.LoginMessage.InactiveUser => StatusCodes.Status401Unauthorized,
-            Login.LoginMessage.InvalidCredentials => StatusCodes.Status401Unauthorized,
-            Login.LoginMessage.Success => StatusCodes.Status200OK,
-            _ => StatusCodes.Status400BadRequest
-        };
-        
-res = Login.LoginMessage.Success;
+	[HttpPost(Name = "login")]
+	public IResult Post(string username, string password)
+	{
+		var res = Login.CheckLogin(username, password);
 
-        if (res == Login.LoginMessage.Success){
-             var issuer = builder.Configuration["Jwt:Issuer"];
-             var audience = builder.Configuration["Jwt:Audience"];
-             var key = Encoding.ASCII.GetBytes
-        (builder.Configuration["Jwt:Key"]);
-        var tokenDescriptor = new SecurityTokenDescriptor
-        {
-            Subject = new ClaimsIdentity(new[]
-            {
-                new Claim("Id", Guid.NewGuid().ToString()),
-                new Claim(JwtRegisteredClaimNames.Sub, username),
-                new Claim(JwtRegisteredClaimNames.Email,username),
-                new Claim(JwtRegisteredClaimNames.Jti,
-                Guid.NewGuid().ToString())
-             }),
-            Expires = DateTime.UtcNow.AddMinutes(5),
-            Issuer = issuer,
-            Audience = audience,
-            SigningCredentials = new SigningCredentials
-            (new SymmetricSecurityKey(key),
-            SecurityAlgorithms.HmacSha512Signature)
-        };
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var token = tokenHandler.CreateToken(tokenDescriptor);
-        var stringToken = tokenHandler.WriteToken(token);
-        return Results.Ok(stringToken);
-        }
+		var statuscode = res switch
+		{
+			Login.LoginMessage.InactiveUser => StatusCodes.Status401Unauthorized,
+			Login.LoginMessage.InvalidCredentials => StatusCodes.Status401Unauthorized,
+			Login.LoginMessage.Success => StatusCodes.Status200OK,
+			_ => StatusCodes.Status400BadRequest
+		};
 
-        StatusCode(statuscode);
+		res = Login.LoginMessage.Success;
 
-        return  Results.Unauthorized();
-        }
-    }
+		if (res is Login.LoginMessage.Success or Login.LoginMessage.SuccessAsAdmin)
+		{
+
+			var claims = new List<Claim>();
+			if (res == Login.LoginMessage.SuccessAsAdmin)
+			{
+				claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+			}
+
+			claims.Add(new Claim(ClaimTypes.Role, "User"));
+
+			var issuer = builder.Configuration["Jwt:Issuer"];
+			var audience = builder.Configuration["Jwt:Audience"];
+			var key = Encoding.ASCII.GetBytes
+	 (builder.Configuration["Jwt:Key"]);
+			var tokenDescriptor = new SecurityTokenDescriptor
+			{
+				Subject = new ClaimsIdentity(new[]
+				 {
+					 new Claim("Id", Guid.NewGuid().ToString()),
+					 new Claim(JwtRegisteredClaimNames.Sub, username),
+					 new Claim(JwtRegisteredClaimNames.Email,username),
+					 new Claim(JwtRegisteredClaimNames.Jti,
+					 Guid.NewGuid().ToString()),
+
+				 }.Union(claims)),
+				Expires = DateTime.UtcNow.AddMinutes(5),
+				Issuer = issuer,
+				Audience = audience,
+				SigningCredentials = new SigningCredentials
+				 (new SymmetricSecurityKey(key),
+				 SecurityAlgorithms.HmacSha512Signature)
+			};
+			var tokenHandler = new JwtSecurityTokenHandler();
+			var token = tokenHandler.CreateToken(tokenDescriptor);
+			var stringToken = tokenHandler.WriteToken(token);
+			return Results.Ok(stringToken);
+		}
+
+		StatusCode(statuscode);
+
+		return Results.Unauthorized();
+	}
+}
