0f9c559e66580a9e5a7e8af8e4a28aa27253fccd
JulianHabenicht <julian.habenicht@gmail.com>
2022-09-16 09:19:44 +0200

    login bootstrap

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,82 +1,35 @@
 // Component imports
 import React from 'react';
-import { Button, Form, Input, Col, Row } from 'antd';
-import {CheckCircleOutlined} from '@ant-design/icons'
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function LoginPage(){
     
-    const onFinish = (values) => {
-        console.log('Success:', values);
-      };
-    
-      const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
-      };
-        
-      
-        return (
-            <>
-        <Row type="flex" justify="center" align="middle" style={{ height: '100px' }}>
-            <Col span={1}>
-                <CheckCircleOutlined style={{ fontSize: '50px' }}/>
-            </Col>
-        </Row>
-        <Row type="flex" justify="center" align="middle">
-            <Col span={12} >           
-                <Form
-                    name="basic"
-                    labelCol={{
-                    span: 8,
-                    }}
-                    wrapperCol={{
-                    span: 8,
-                    }}
-                    
-                    onFinish={onFinish}
-                    onFinishFailed={onFinishFailed}
-                    autoComplete="off"
-                >
-                    <Form.Item
-                    label="Benutzername"
-                    name="username"
-                    rules={[
-                        {
-                        required: true,
-                        message: 'Bitte geben Sie Ihren Benutzernamen ein!',
-                        },
-                    ]}
-                    >
-                    <Input />
-                    </Form.Item>
-            
-                    <Form.Item
-                    label="Passwort"
-                    name="password"
-                    rules={[
-                        {
-                        required: true,
-                        message: 'Bitte geben Sie Ihr Passwort ein!',
-                        },
-                    ]}
-                    >
-                    <Input.Password />
-                    </Form.Item> 
-                
-            
-                    <Form.Item
-                    wrapperCol={{
-                        offset: 8,
-                        span: 16,
-                    }}
-                    >
-                    <Button type="primary" htmlType="submit">
-                        Einloggen
-                    </Button>
-                    </Form.Item>
-                </Form>
-            </Col>
-        </Row>
-        </>
+    return(
+        <Container>
+            <Row className="justify-content-center align-items-center" style={{ height: '100vh' }}>
+                <Col xs={6} lg={3}>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Benutzername</Form.Label>
+                            <Form.Control type="text"/>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Passwort</Form.Label>
+                            <Form.Control type="password"/>
+                        </Form.Group>                
+                        <Button variant="primary" type="submit">
+                            Einloggen
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
