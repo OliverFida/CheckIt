568a6cbc68d9ce1e686420ecbe5f491c25027629
FireFerrret <markus.domino@hotmail.de>
2022-09-16 09:39:12 +0200

    Raum und Booking Endpunkte

--- backend/Controllers/bookingController.cs
+++ backend/Controllers/bookingController.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using static awl_raumreservierung.Controllers.adminController;
 
 namespace awl_raumreservierung.Controllers;
 
@@ -6,15 +8,74 @@ namespace awl_raumreservierung.Controllers;
 [Route("[controller]")]
 public class bookingController : ControllerBase
 {
-    private readonly ILogger<bookingController> _logger;
+	private readonly ILogger<bookingController> _logger;
 
-    public bookingController(ILogger<bookingController> logger)
-    {
-        _logger = logger;
-    }
+	public bookingController(ILogger<bookingController> logger)
+	{
+		_logger = logger;
+	}
 
-    [HttpPost()]
-    public User Post(string username, string password){
-        return new User(); //ohne pw und logondaten
-    }
+	[HttpGet("roomBookings")]
+	[Authorize]
+	public Booking[] Get(int roomId)
+	{
+		var db = new checkITContext();
+		return db.Bookings.Where(s => s.Room == roomId).ToArray();
+	}
+	[HttpPut("book")]
+	[Authorize]
+	public StatusCodeResult book(int roomId, string username, DateTime startTime, DateTime endTime, DateTime createTime, int createdBy)
+	{
+		var userId = getUserId(username);
+		if (userId < 0)
+		{
+			return StatusCode(StatusCodes.Status404NotFound);
+		}
+		var db = new checkITContext();
+		var booking = new Booking(startTime, endTime, roomId, userId, createTime, createdBy);
+
+		db.Bookings.Add(booking);
+		db.SaveChanges();
+		return StatusCode(StatusCodes.Status201Created);
+	}
+
+	[HttpPut("remove")]
+	[Authorize]
+	public StatusCodeResult remove(int bookingId)
+	{
+		var db = new checkITContext();
+		var booking = db.Bookings.Where(b => b.Id == bookingId).FirstOrDefault();
+		if (booking != null)
+		{
+			db.Bookings.Remove(booking);
+			db.SaveChanges();
+		}
+		return StatusCode(StatusCodes.Status200OK);
+	}
+	[HttpPut("bookAsAdmin")]
+	[Authorize(Roles = "Adminstrator")]
+	public StatusCodeResult bookAsAdmin(int roomId, string username, DateTime startTime, DateTime endTime, DateTime createTime, int createdBy)
+	{
+		var userId = getUserId(username);
+		if (userId < 0)
+		{
+			return StatusCode(StatusCodes.Status404NotFound);
+		}
+		var db = new checkITContext();
+		var booking = new Booking(startTime, endTime, roomId, userId, createTime, createdBy);
+		db.Bookings.Add(booking);
+		db.SaveChanges();
+		return StatusCode(StatusCodes.Status201Created);
+	}
+	private long getUserId(string username)
+	{
+		var db = new checkITContext();
+		var user = db.Users.Where(u => u.Username == username).FirstOrDefault();
+		long id = -1;
+		if (user != null)
+		{
+			id = user.Id;
+		}
+		return id;
+	}
 }
--- backend/classes/db/Room.cs
+++ backend/classes/db/Room.cs
@@ -8,5 +8,11 @@ namespace awl_raumreservierung
         public long Id { get; set; }
         public string? Number { get; set; }
         public string? Name { get; set; }
-    }
+
+      public Room(string? number, string? name)
+      {
+         Number = number;
+         Name = name;
+      }
+   }
 }
