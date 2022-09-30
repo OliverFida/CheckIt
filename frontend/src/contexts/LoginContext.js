cca7be70728e48b02d82218ded98b92784288af7
OliverFida <oliver.fi99@gmail.com>
2022-09-30 10:07:31 +0200

    Login WIP

---
+++ frontend/src/contexts/LoginContext.js
@@ -0,0 +1,13 @@
+import React, {useState, createContext} from 'react';
+
+export const LoginContext = createContext(null);
+
+export default function LoginContextProvider({children}){
+    const [loginContext, setLoginContext] = useState({userId: null, loginToken: null});
+
+    return(
+        <LoginContext.Provider value={{loginContext, setLoginContext}}>
+            {children}
+        </LoginContext.Provider>
+    );
+}
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,13 +1,35 @@
 // Component imports
-import React from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 import {Button, Form, Container, Row, Col} from 'react-bootstrap';
+import LoginContextProvider, { LoginContext } from '../contexts/LoginContext';
+// API imports
+import LoginAPI from '../api/login';
 
 export default function LoginPage(){
+    return(
+        <LoginContextProvider>
+            <LoginPageContent />
+        </LoginContextProvider>
+    )
+};
+
+function LoginPageContent(){
+    const {loginContext} = useContext(LoginContext);
+    const [values, setValues] = useState({username: "", password: ""});
     const navigate = useNavigate();
 
+    const onValueChange = (event) => {
+        setValues({...values, [event.target.name]: event.target.value});
+    };
+
     const onLogin = () => {
-        navigate("/home");
+        var response = LoginAPI.login(values.username, values.password);
+        if(response.statusCode === 200){
+            navigate("/home");
+        }else{
+            setValues({...values, password: ""});
+        }
     };
 
     return(
@@ -17,21 +39,21 @@ export default function LoginPage(){
                     <Form>
                         <Form.Group className="mb-3" controlId="formEmail">
                             <Form.Label>Benutzername</Form.Label>
-                            <Form.Control type="text"/>
+                            <Form.Control type="text" onChange={onValueChange} name="username" value={values.username} />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formPassword">
                             <Form.Label>Passwort</Form.Label>
-                            <Form.Control type="password"/>
+                            <Form.Control type="password" onChange={onValueChange} name="password" value={values.password} />
                         </Form.Group>                
-                        <Button variant="primary" type="submit" className="mx-auto d-block" onClick={onLogin}>
+                        <Button variant="primary" className="mx-auto d-block" onClick={onLogin}>
                             Einloggen
                         </Button>
                     </Form>
+                    <p><b>Debug Username:</b> dageorg<br />
+                    <b>Debug Password:</b> linux</p>
                 </Col>
             </Row>
         </Container>
     );
-};
-
-      
+}
