cd4052a601b255c74f97f3ad6bd99a89afc0dbc2
tkolasinski <thomaskolasinski@users.noreply.github.com>
2022-09-30 07:40:53 +0200

    Revert ""
    This reverts commit 37eb39682987cad85f057f8a170bced3e4538b07.

---
+++ backend/classes/public/PublicBooking.cs
@@ -0,0 +1,34 @@
+﻿namespace awl_raumreservierung
+{
+    public class PublicBooking
+    {
+        public long Id { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? StartTimeUTC
+        {
+            get { return StartTime.ToUtc(); }
+        }
+        public DateTime? EndTime { get; set; }
+        public DateTime? EndTimeUTC
+        {
+            get { return EndTime.ToUtc(); }
+        }
+        public PublicRoom Room { get; set; }
+        public PublicUser User { get; set; }
+        public DateTime? CreateTime { get; set; }
+        public DateTime? CreateTimeUTC
+        {
+            get { return CreateTime.ToUtc(); }
+        }
+
+        public PublicBooking(Booking booking)
+        {
+            this.Id = booking.Id;
+            this.StartTime = booking.StartTime;
+            this.EndTime = booking.EndTime;
+            this.Room = Helpers.GetRoom(booking.Room).ToPublicRoom();
+            this.User = Helpers.GetUser(booking.UserId.ToInt()).ToPublicUser();
+            this.CreateTime = booking.CreateTime;
+        }
+    }
+}
