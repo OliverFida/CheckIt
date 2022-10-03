8df7c5094873e38d73d819b5555d218c7805a378
Futagirl <anime@incest.rocks>
2022-10-03 12:10:03 +0200

    Comments

--- backend/classes/public/PublicRoom.cs
+++ backend/classes/public/PublicRoom.cs
@@ -1,12 +1,32 @@
 ﻿namespace awl_raumreservierung
 {
-    // TK - Ja ich weiß die klasse is redundant aber sollten
-    // vielleicht irgendwann mal sachen dazukommen ist das praktisch
+/// <summary>
+/// Öffentliche Daten eines Raums
+/// </summary>
     public class PublicRoom
     {
+        /// <summary>
+        /// ID
+        /// </summary>
+        /// <value></value>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Raumnummer
+        /// </summary>
+        /// <value></value>
         public string? Number { get; set; }
+
+        /// <summary>
+        /// Raumname
+        /// </summary>
+        /// <value></value>
         public string? Name { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="room"></param>
         public PublicRoom(Room? room)
         {
             if(room != null)
