13658e5a27e3715457a31bd8ea09162b1a4486dd
OliverFida <oliver.fi99@gmail.com>
2022-10-05 16:00:05 +0200

    Backend Config

--- frontend/src/api/base.js
+++ frontend/src/api/base.js
@@ -1,9 +1,16 @@
 const axios = require('axios').default;
 
 const SERVER_CONF = {
+    PROTOCOL: "https",
     ADDRESS: "10.0.5.57",
     PORT: "7061"
 };
+
+// const SERVER_CONF = {
+//     PROTOCOL: "http",
+//     ADDRESS: "10.0.3.19",
+//     PORT: "5000"
+// };
 
 async function apiRequest(path, method, data){
     var returnVal = null;
@@ -11,7 +18,7 @@ async function apiRequest(path, method, data){
     var token = await localStorage.getItem('loginToken');
 
     await axios({
-        url: `https://${SERVER_CONF.ADDRESS}:${SERVER_CONF.PORT}/${path}`,
+        url: `${SERVER_CONF.PROTOCOL}://${SERVER_CONF.ADDRESS}:${SERVER_CONF.PORT}/${path}`,
         method: method,
         data: data,
         headers: {
