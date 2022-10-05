d63de78c16a4099e30defac876c1a0201243e6aa
OliverFida <oliver.fi99@gmail.com>
2022-10-05 15:51:07 +0200

    AppNav Admin Teil ausgelagert

--- frontend/src/pages/components/AppNavUser.js
+++ frontend/src/pages/components/AppNavUser.js
@@ -5,20 +5,12 @@ import { useNavigate } from 'react-router-dom';
 
 import LoginAPI from '../../api/login';
 
-export default function AppNavBooking(){
+export default function AppNavUser(){
     const navigate = useNavigate();
     
     const clickProfile = () => {
         navigate("/user");
     };
-
-    const clickUserEdit = () => {
-        navigate("/user-edit");
-    };
-
-    const clickRooms = () => {
-        navigate("/rooms");
-    }
 
     const onLogout = () => {
         LoginAPI.logout();
@@ -28,9 +20,27 @@ export default function AppNavBooking(){
     return(
         <NavDropdown title="Vorname Nachname" align={'end'}>
             <NavDropdown.Item onClick={clickProfile}>Profil</NavDropdown.Item>
-            {localStorage.getItem("loginAdmin") === "true" ? <NavDropdown.Item onClick={clickUserEdit}>Benutzerverwaltung</NavDropdown.Item> : null}
-            {localStorage.getItem("loginAdmin") === "true" ? <NavDropdown.Item onClick={clickRooms}>Raumverwaltung</NavDropdown.Item> : null}
+            {localStorage.getItem("loginAdmin") === "true" ? <AppNavUserAdmin /> : null}
             <NavDropdown.Item onClick={onLogout} className="border-top">Abmelden</NavDropdown.Item>
         </NavDropdown>
     );
 };
+
+function AppNavUserAdmin(){
+    const navigate = useNavigate();
+    
+    const clickUserEdit = () => {
+        navigate("/user-edit");
+    };
+
+    const clickRooms = () => {
+        navigate("/rooms");
+    }
+
+    return(
+        <>
+        <NavDropdown.Item onClick={clickUserEdit}>Benutzerverwaltung</NavDropdown.Item>
+        <NavDropdown.Item onClick={clickRooms}>Raumverwaltung</NavDropdown.Item>
+        </>
+    );
+}
