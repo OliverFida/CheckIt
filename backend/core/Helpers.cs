284cb7ba44af3caf4b4afbee6afe838028f8cdf5
Futagirl <anime@incest.rocks>
2022-10-06 12:54:00 +0200

    Entnullifiziert

--- backend/core/Helpers.cs
+++ backend/core/Helpers.cs
@@ -1,5 +1,7 @@
+using System.Diagnostics;
 using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace awl_raumreservierung
 {
@@ -13,17 +15,37 @@ namespace awl_raumreservierung
 		/// </summary>
 		/// <param name="username">Username</param>
 		/// <returns></returns>
-		public static User? GetUser(string username)
+		public static User GetUser(string username)
 		{
-			try
+			using checkITContext ctx = new();
+			var user = ctx.Users.Where(u => u.Username.ToLower().Trim() == username.ToLower().Trim()).FirstOrDefault();
+
+			if (user is null)
 			{
-				using checkITContext ctx = new();
-				return ctx.Users.Where(u => u.Username.ToLower().Trim() == username.ToLower().Trim()).FirstOrDefault();
+				throw new Exception($"Zum Namen {username} wurde kein Benutzer gefunden.");
 			}
-			catch
-			{
-				return null;
-			}
+
+			return user;
+		}
+
+		/// <summary>
+		/// Checkt, ob ein Nutzer existiert
+		/// </summary>
+		/// <param name="username">Benutzername</param>
+		/// <returns></returns>
+		public static bool DoesUserExist(string username)
+		{
+			return new checkITContext().Users.Any(u => u.Username == username);
+		}
+
+		/// <summary>
+		/// Checkt, ob ein Nutzer existiert
+		/// </summary>
+		/// <param name="userID">User-ID</param>
+		/// <returns></returns>
+		public static bool DoesUserExist(int userID)
+		{
+			return new checkITContext().Users.Find(userID) is null;
 		}
 
 		/// <summary>
@@ -31,17 +53,17 @@ namespace awl_raumreservierung
 		/// </summary>
 		/// <param name="id">User-ID</param>
 		/// <returns></returns>
-		public static User? GetUser(int id)
+		public static User GetUser(int id)
 		{
-			try
+			using checkITContext ctx = new();
+			var user = ctx.Users.Where(u => u.Id == id).FirstOrDefault();
+
+			if (user is null)
 			{
-				using checkITContext ctx = new();
-				return ctx.Users.Where(u => u.Id == id).FirstOrDefault();
+				throw new Exception($"Zur ID {id} wurde kein Benutzer gefunden.");
 			}
-			catch
-			{
-				return null;
-			}
+
+			return user;
 		}
 
 		/// <summary>
@@ -49,17 +71,17 @@ namespace awl_raumreservierung
 		/// </summary>
 		/// <param name="id">ID des Raums</param>
 		/// <returns></returns>
-		public static Room? GetRoom(long id)
+		public static Room GetRoom(long id)
 		{
-			try
+			using checkITContext ctx = new();
+			var room = ctx.Rooms.Where(r => r.Id == id).FirstOrDefault();
+
+			if (room is null)
 			{
-				using checkITContext ctx = new();
-				return ctx.Rooms.Where(r => r.Id == id).FirstOrDefault();
+				throw new Exception($"Zur ID {id} wurde kein Raum gefunden.");
 			}
-			catch
-			{
-				return null;
-			}
+
+			return room;
 		}
 
 		/// <summary>
@@ -67,17 +89,17 @@ namespace awl_raumreservierung
 		/// </summary>
 		/// <param name="id">Booking-ID</param>
 		/// <returns></returns>
-		public static Booking? GetBooking(long id)
+		public static Booking GetBooking(long id)
 		{
-			try
+			using checkITContext ctx = new();
+			var booking = ctx.Bookings.Where(b => b.Id == id).FirstOrDefault();
+
+			if (booking is null)
 			{
-				using checkITContext ctx = new();
-				return ctx.Bookings.Where(b => b.Id == id).FirstOrDefault();
+				throw new Exception($"Zur ID {id} wurde keine Buchung gefunden.");
 			}
-			catch
-			{
-				return null;
-			}
+
+			return booking;
 		}
 
 		/// <summary>
@@ -88,12 +110,8 @@ namespace awl_raumreservierung
 		public static bool BookingOverlaps(CreateBookingModel model)
 		{
 			var room = GetRoom(model.RoomID);
-			if (room is null)
-			{
-				return false;
-			}
 
-			bool overlapsWithOtherBookings = room.GetBookings().Any(b => b.StartTime <= model.EndTime.Subtract(new TimeSpan(0,0,1)) && model.StartTime <= b.EndTime);
+			bool overlapsWithOtherBookings = room.GetBookings().Any(b => b.StartTime <= model.EndTime.Subtract(new TimeSpan(0, 0, 1)) && model.StartTime <= b.EndTime);
 			return overlapsWithOtherBookings;
 		}
 
@@ -105,14 +123,11 @@ namespace awl_raumreservierung
 		public static bool BookingOverlaps(Booking booking)
 		{
 			var room = GetRoom(booking.Id);
-			if (room is null)
-			{
-				return false;
-			}
 
-			bool overlapsWithOtherBookings = room.GetBookings().Any(b => b.StartTime <= booking.EndTime?.Subtract(new TimeSpan(0, 0, 1)) && booking.StartTime <= b.EndTime);
+			bool overlapsWithOtherBookings = room.GetBookings().Any(b => b.StartTime <= booking.EndTime.Subtract(new TimeSpan(0, 0, 1)) && booking.StartTime <= b.EndTime);
 			return overlapsWithOtherBookings;
 		}
+
 		/// <summary>
 		/// Checkt ob die Zeiten einer Buchung valide sind.
 		/// </summary>
@@ -128,9 +143,10 @@ namespace awl_raumreservierung
 			if (BookingOverlaps(booking))
 			{
 				throw new ArgumentException("Buchung überlappt mit anderer Buchung");
-		}
+			}
 			return true;
 		}
+
 		/// <summary>
 		/// Setzt den Aktivstatus eines Users
 		/// </summary>
