fbd779a25433436f57adfc0540a4bd28da336601
Futagirl <anime@incest.rocks>
2022-10-11 10:28:53 +0200

    Installdoc

--- backend/tools/build.sh
+++ backend/tools/build.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 docker build -t checkit .
-docker save checkit > checkit.docker
+docker save checkit > tools/release_files/checkit.docker
 tar -cv tools/release_files/* -f out/checkit_release.tgz
+
