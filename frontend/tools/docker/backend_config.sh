fcf5cd7613ded20ac0d79695960f61ace0d73c5b
Futagirl <anime@incest.rocks>
2022-11-03 15:04:24 +0100

    backend config sh

--- frontend/tools/docker/backend_config.sh
+++ frontend/tools/docker/backend_config.sh
@@ -1,1 +1,9 @@
-echo "{\"PROTOCOL\": $CHECKIT_BACKEND_PROTOCOL, \"ADDRESS\": $CHECKIT_BACKEND_ADDRESS, \"PORT\": $CHECKIT_BACKEND_PORT}" > /www/backend_config.json
+#! /bin/bash
+if [ -z $CHECKIT_BACKEND_PROTOCOL ]
+then
+	echo "{\"PROTOCOL\": $CHECKIT_BACKEND_PROTOCOL, \"ADDRESS\": $CHECKIT_BACKEND_ADDRESS, \"PORT\": $CHECKIT_BACKEND_PORT}" > /www/backend_config.json
+elif [ -f /www/backend_config.json ]
+then
+	 rm /www/backend_config.json
+fi
+
