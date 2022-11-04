82c59c0c8ee786189bee8324bab2e2ed7e8a4886
Futagirl <anime@incest.rocks>
2022-11-04 10:05:34 +0100

    build

--- build_complete.sh
+++ build_complete.sh
@@ -11,7 +11,7 @@ mv backend/tools/release_files/checkit_backend.docker release_files/
 mv frontend/tools/release_files/checkit_frontend.docker release_files/
 
 cd release_files
-tar -cv * -f ../checkit_release.tgz
+# tar -cv * -f ../checkit_release.tgz
 
 docker push ghcr.io/oliverfida/checkit_backend:latest
 docker push ghcr.io/oliverfida/checkit_frontend:latest
